4e5cd1ba6878c33eb1bbdf9ef3b84f229895b2f7
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-08-31 13:18:57 -0400

    created express uv and bare

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import http from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import path from "node:path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+// ADD MORE IF YOU HAVE MORE PAGES
+
+
+// Currently do not have a 404 page
+/* app.get("/*", (req, res) => {
+  res.redirect("/404");
+});
+*/
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`monkeyz listening on port ${process.env.PORT}`);
+});
+
+server.listen({
+  port: process.env.PORT,
+});
